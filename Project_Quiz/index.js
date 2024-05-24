8bf21a6f78373049b25e14558fb463eb54bf2940
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-24 14:02:29 -0700

    Update index.js

--- Project_Quiz/index.js
+++ Project_Quiz/index.js
@@ -1,73 +1,85 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-console.log(chalk.greenBright("*********", "TypeScript Quiz - MCQ - Multiple Choice Questions", "**********"));
+let ans1 = "A. A superset of JavaScript that adds static typing";
+let ans2 = "B. A special type that defines a set of named constants";
+let ans3 = "C. export function myFunction() { }";
+let ans4 = "A. A type that can hold one of several types";
+let ans5 = "A. function identity<T>(arg: T): T { return arg; }";
+let ans6 = "A. class MyClass implements MyInterface { }";
+let ans7 = "A. let num: number = 5;";
+let ans8 = "A. To define a blueprint for classes";
+let ans9 = `"A. let someValue: any = "this is a string"; let strength: number = (<string>someValue).length;"`;
+let ans10 = "A. tsc";
 let Quiz = await inquirer.prompt([
     {
         name: "q1",
         type: "list",
-        message: "1. What is TypeScript primarily used for?",
-        choices: ["A. Asynchronous operations", "B. Static Typing", "C. Memory Management", "D. Dynamic Typing"],
+        message: "Q1: What is TypeScript ? ",
+        choices: ["A. A superset of JavaScript that adds static typing", "B. A framework for building web applications", "C. A new programming language unrelated to JavaScript",
+            "D. A library for handling asynchronous operations"],
     },
     {
         name: "q2",
         type: "list",
-        message: "2. Which of the following is NOT a valid TypeScript data type?",
-        choices: ["A. tuple", "B. any", "C. void", "D. dynamic"],
+        message: "Q2: What is an enum in TypeScript?",
+        choices: ["A. A way to declare private variables", "B. A special type that defines a set of named constants", "C. A variable that holds multiple values", "D. A function that performs asynchronous operations"],
     },
     {
         name: "q3",
         type: "list",
-        message: "3. How do you specify that a function does not return anything in TypeScript?",
-        choices: ["A. function myFunc(): None", "B. function myFunc(): undefined", "C. function myFunc(): null", "D. function myFunc(): void"],
+        message: "Q3: How do you export a function from a module in TypeScript?",
+        choices: ["A. module. exports = myFunction;", "B. public function myFunction() { }", "C. export function myFunction() { }", "D. exports.myFunction = function() { }"],
     },
     {
         name: "q4",
         type: "list",
-        message: "4. How do you define a custom type in TypeScript?",
-        choices: ["A. interface", "B. type", "C. Both A and B", "D. typedef"],
+        message: "Q4 : What is a union type in TypeScript?",
+        choices: ["A. A type that can hold one of several types", "B. A type that combines multiple interfaces", "C. A type used for asynchronous operations", "D. A type that provides runtime type checking"],
     },
     {
         name: "q5",
         type: "list",
-        message: "5. What is the primary purpose of TypeScript interfaces?",
-        choices: ["A. To manage asynchronous code", "B. To generate HTML templates", "C. To create new classes", "D. To describe the shape of an object"],
+        message: "Q5: Which of the following is a correct use of generics in TypeScript?",
+        choices: ["A. function identity<T>(arg: T): T { return arg; }", "B. function identity(arg: any): any { return arg; }", "C. function identity<T>(arg): T { return arg; }", "D. function identity(arg: T): T { return arg; }"],
     },
     {
         name: "q6",
         type: "list",
-        message: "6. How do you define an optional parameter in the TypeScript function?",
-        choices: ["A. function foo(param string?)", "B. function foo(param?: string)", "C. function foo(param string=)", "D. function foo(param: string?)"],
+        message: "Q6: How do you declare a class in TypeScript that implements an interface?",
+        choices: ["A. class MyClass implements MyInterface { }", "B. class MyClass extends MyInterface { }", "C. class MyClass implementsClass MyInterface { }", "D. class MyClass implementsInterface MyInterface { }"],
     },
     {
         name: "q7",
         type: "list",
-        message: "7. Which of the following TypeScript types can the unknown type be assigned to without type assertion?",
-        choices: ["A. any", "B. number", "C. string", "D. void"],
+        message: "Q7: Which of the following is the correct way to declare a variable with a number type in TypeScript?",
+        choices: ["A. let num: number = 5;", `"B. let num = "5;"`, "C. var num: 5 = number;", "D. num: let number = 5;"],
     },
     {
         name: "q8",
         type: "list",
-        message: "8. Which TypeScript feature allows for declaring new names for existing types?",
-        choices: ["A. Interfaces", "B. Decorators", "C. Aliases", "D. Enums"],
+        message: "Q8: What is the purpose of an interface in TypeScript?",
+        choices: ["A. To define a blueprint for classes", "B. To provide runtime type checking", "C. To enforce security policies", "D. To enhance the performance of applications"],
     },
     {
         name: "q9",
         type: "list",
-        message: "9. How do you declare a class in TypeScript?",
-        choices: ["A. class ClassName {}", "B. new Class ClassName {}", "C. def ClassName:", "D. object ClassName {}"],
+        message: "Q9 : Which syntax is used for type assertions in TypeScript?",
+        choices: [`"A. let someValue: any = "this is a string"; let strLength: number = (<string>someValue).length;"`, `"B. let someValue: any = "this is a string"; let strLength: number = (string)someValue.length;"`, `"C. let someValue: any = "this is a string"; let strLength: number = (someValue<string>).length;"`, `"D. let someValue: any = "this is a string"; let strLength: number = (someValue).length<string>;"`],
     },
     {
         name: "q10",
         type: "list",
-        message: "10. How do you define an array of strings in TypeScript?",
-        choices: ["A. Array<string>", "B. List<string>", "C. Both A and D", "D. string[]"],
+        message: "Q10: Which command is used to compile a TypeScript file?",
+        choices: ["A. tsc", "B. node", "C. npm run", "D. ts-node"],
     },
 ]);
-if (Quiz.q1 == "B. Static Typing" || Quiz.q2 == "D. dynamic" || Quiz.q3 == "D. function myFunc(): void" || Quiz.q4 == "C. Both A and B" || Quiz.q5 == "D. To describe the shape of an object" || Quiz.q6 == "B. function foo(param?: string)" || Quiz.q7 == "A. any" || Quiz.q8 == "C. Aliases" || Quiz.q9 == "A. class ClassName {}" || Quiz.q10 == "C. Both A and D") {
+if (Quiz.q1 == "A. A superset of JavaScript that adds static typing" || Quiz.q2 == "B. A special type that defines a set of named constants" || Quiz.q3 == "C. export function myFunction() { }" || Quiz.q4 == "A. A type that can hold one of several types" || Quiz.q5 == "A. function identity<T>(arg: T): T { return arg; }" || Quiz.q6 == "A. class MyClass implements MyInterface { }" || Quiz.q7 == "A. let num: number = 5;" || Quiz.q8 == "A. To define a blueprint for classes" || Quiz.q9 == `"A. let someValue: any = "this is a string"; let strLength: number = (<string>someValue).length;"` || Quiz.q10 == "A. tsc") {
     console.log("\n");
-    console.log(chalk.yellowBright("**********", "The result of the quiz:", "************"));
-    console.log(chalk.green(`1)${"B. Static Typing"} ${"\n"}2)${"D. dynamic"} ${"\n"}3)${"D. function myFunc(): void"}  ${"\n"}4)4{"C. Both A and B"}  ${"\n"}5)${"D. To describe the shape of an object"}  ${"\n"}6){"B. function foo(param?: string)"}  ${"\n"}7)${"A. any"}   ${"\n"}8)${"C. Aliases"}  ${"\n"}9)${"A. class ClassName {}"}  ${"\n"}10)${"C. Both A and D"}`));
+    console.log(chalk.yellow bright("Check your correct answer!"));
+    console.log(chalk.greenlight("Q1 : Correct answer is = ", ans1 + "\n" + "Q2 : Correct answer is = ", ans2 + "\n" + "Q3 : Correct answer is = ", ans3 + "Q4 : Correct answer is = ", ans4 + "\n" + "Q5 : Correct answer is = ", ans5 + "\n" + "Q6 : Correct answer is = ", ans6 + "\n" + "Q7 : Correct answer is = ", ans7 + "\n" + "Q8 : Correct answer is = ", ans8 + "\n" + "Q9 : Correct answer is = ", ans9 + "\n" + "Q10 : Correct answer is = ", ans10));
 }
 else {
     console.log("Try again later!");
 }
+
